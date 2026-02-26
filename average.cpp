c13b74149bf36fed4b3e8ccfedfe29d64febfaf1
Traipop <traipopwichiansarn@gmail.com>
2026-02-27 00:24:57 +0700

    lab21_1 completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,21 @@
+    #include <iostream>
+    #include <cstdlib>
+    using namespace std;
 
+    int main(int argc , char *argv[]){
+        double sum = 0;
+        if(argc != 1){
+            for( int i = 0 ; i < argc ; i++){
+                sum += atoi(argv[i]);
+            }
+            double avg = sum/(argc-1);
+            cout << "\n---------------------------------\n";
+            cout << "Average of " << argc - 1 <<" numbers = " << avg ;
+            cout << "\n---------------------------------";
+        } else {
+            cout << "Please input numbers to find average.";
+        }
+        return 0;
+
+
+    }
